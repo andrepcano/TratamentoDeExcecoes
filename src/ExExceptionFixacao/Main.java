540e20b4abe7d67ba9b32ef2603ed4380301cb83
André <andrepcano@gmail.com>
2026-05-13 16:20:40 -0300

    feat: exercicio implementando o uso de exceçÃões personalizadas da forma correta

---
+++ src/ExExceptionFixacao/Main.java
@@ -0,0 +1,39 @@
+package ExExceptionFixacao;
+
+import ExExceptionFixacao.model.entities.Account;
+import ExExceptionFixacao.model.exceptions.WithdrawException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            String holder = sc.next();
+            sc.next();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.print("Enter amount for withdraw: ");
+            account.withdraw(sc.nextDouble());
+
+            System.out.println("New balance: " + account.getBalance());
+
+        }catch (WithdrawException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error!");
+        }
+
+        sc.close();
+    }
+}
